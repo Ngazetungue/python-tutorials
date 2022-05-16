aa7f4ef5b68f87767d027f50c42093c9e5361923
Ngazetungue <nmuheuez@gmail.com>
2022-05-16 12:48:09 +0200

    Update weather_api.py

--- weather_api.py
+++ weather_api.py
@@ -1,3 +1,9 @@
 import requests
-import os
-from datetime import datetime
+
+api_key = 'MY API KEY'
+
+my_input = input("Enter city Name: ")
+
+weather_data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={api_key}')
+
+print(weather_data.json())
